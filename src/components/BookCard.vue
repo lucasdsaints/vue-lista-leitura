1896b09b58b311afae292c7a2c795f6049a84743
Lucas Santos <lucas.assis.santos96@gmail.com>
2020-02-27 22:54:11 -0300

    inicio projeto

---
+++ src/components/BookCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-col>
+      <b-card
+        :title="book.title"
+        :img-src="book.imageUrl"
+        img-alt="capa livro"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2">
+        <b-card-text>
+          {{ book.description }}
+        </b-card-text>
+        <b-button variant="primary">Detalhes</b-button>
+      </b-card>
+    </b-col>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    book: Object
+  }
+}
+</script>
+
+<style>
+ .card-img-top {
+   height: 200px;
+   object-fit: cover;
+ }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <BookCard v-for="book in displayBooks" :book="book" :key="book.id"/>
+    </b-row>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+      @input="paginate(currentPage)"
+    ></b-pagination>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import BookCard from "@/components/BookCard";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    BookCard
+  },
+  data () {
+    return {
+      books: [],
+      displayBooks: [],
+      currentPage: 1,
+      rows: 1,
+      perPage: 3
+    };
+  },
+  methods: {
+    async fetchData () {
+      const res = await fetch("books.json");
+      this.books = await res.json();
+      this.displayBooks = this.books.slice(0, 3);
+      this.rows = this.books.length;
+    },
+    paginate (currentPage) {
+      const start = (currentPage - 1) * this.perPage;
+      this.displayBooks = this.books.slice(start, start+3);
+    }
+  },
+  mounted () {
+    this.fetchData();
   }
 };
 </script>
